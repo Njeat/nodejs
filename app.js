02e403f482d1aecbade2529400940a85447f2f3f
parktj <ekswn120@gmail.com>
2022-03-09 19:20:43 +0900

    clean the code using ejs

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-    res.send("여기는 루트입니다.")
-});
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get("/login", (req, res) => {
-    res.send("여기는 로그인 화면입니다.")
-})
+app.get("/", function (req, res) {});
+
+app.get("/login", (req, res) => {});
 
 app.listen(3000, function () {
     console.log("서버 가동");
